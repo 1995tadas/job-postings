4e50356d325d4f2686ec082d5a477fe82eb31dbc
1995tadas <1995tadas@gmail.com>
2020-11-26 13:36:32 +0200

    Empty input id check in form input component

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,2 +1,2 @@
-<input class="border py-2 px-3 text-grey-darkest w-full" type="text" name="{{$name}}" value="{{$value}}" id="{{$id}}"
-       maxlength="255" {{$required?'required':''}}>
+<input class="border py-2 px-3 text-grey-darkest w-full" type="text" name="{{$name}}" value="{{$value}}"
+       @if(isset($id)) id="{{$id}}" @endif maxlength="255" {{$required?'required':''}}>
